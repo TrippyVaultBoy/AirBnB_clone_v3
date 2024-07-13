1fae03ed7ee3b1ad2830695849023961f8c47387
nooboftime <tamara_walling@yahoo.com>
2024-07-13 02:05:48 +0000

    object type index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,16 @@ from flask import Flask, jsonify
 def status_OK():
     """returns a JSON status"""
     return jsonify({"status": "OK", "code": 200}), 200
+
+
+@app_views.route("/api/v1/stats")
+def stats():
+    """endpoint that retrieves the number of each objects by type"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Places"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+})
