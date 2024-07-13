eda483fe7352f0ffb87c04234efa6fbb7653400f
nooboftime <tamara_walling@yahoo.com>
2024-07-13 02:21:46 +0000

    end blank line in index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status_OK():
     """returns a JSON status"""
