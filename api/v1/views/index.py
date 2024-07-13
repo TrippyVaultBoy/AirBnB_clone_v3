ecefd3a6f3100c346b6b07cecb44b7fa977c56d5
nooboftime <tamara_walling@yahoo.com>
2024-07-13 02:10:07 +0000

    accidental s in places needed to be place in index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,7 +16,7 @@ def stats():
     return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
-        "places": storage.count("Places"),
+        "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
         "users": storage.count("User"),
