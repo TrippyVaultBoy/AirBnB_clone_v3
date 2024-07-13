d0a596c69d7d4c1efed80593bffaef3ac976697b
Tanner Saint <tsaint2004@gmail.com>
2024-07-13 01:07:14 +0000

    added jsonify call

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """creates a route to status"""
 from api.v1.views import app_views
+from flask import Flask, jsonify
 
 @app_views.route("/status")
 def status_OK():
     """returns a JSON status"""
-    status = {"status": "OK"}
-    return (status.json())
+    return jsonify({"status": "OK"})
