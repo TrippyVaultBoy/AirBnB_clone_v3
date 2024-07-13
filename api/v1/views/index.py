bc257dfb50151bb090d3ab79cb38c607f5ee84f4
Tanner Saint <tsaint2004@gmail.com>
2024-07-13 00:56:33 +0000

    added code to index.py and __init__.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+from api.v1.views import app_views
+
+@app_views.route("/status")
+def status_OK():
+    """returns a JSON status"""
+    status = {"status": "OK"}
+    return (status.json())
