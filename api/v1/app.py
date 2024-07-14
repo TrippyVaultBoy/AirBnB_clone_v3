5bc38991a065f1b5979396cf0158880e989c446b
nooboftime <tamara_walling@yahoo.com>
2024-07-14 11:50:11 -0500

    import jsonify in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """It’s time to start your API!"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
