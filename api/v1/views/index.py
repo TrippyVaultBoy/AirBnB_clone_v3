542c5e923bc4bbb69bef3bf28a13c2214660a436
nooboftime <tamara_walling@yahoo.com>
2024-07-14 19:33:45 -0500

    changed  f to F in flask

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """creates a route to status"""
 from api.v1.views import app_views
-from flask import flask, jsonify
+from flask import Flask, jsonify
 from models import storage
 from models.state import State
 from models.city import City
