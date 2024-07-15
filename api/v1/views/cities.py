a40ff72eb557723891e6a8fba9b24bcd2cc95df2
nooboftime <tamara_walling@yahoo.com>
2024-07-14 20:29:14 -0500

    deleted semicolon on line 46

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -43,7 +43,7 @@ def create_city(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    data = request.get_json():
+    data = request.get_json()
     if not data:
         abort(400, 'Not a JSON')
     if 'name' not in data:
