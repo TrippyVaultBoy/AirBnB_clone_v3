1610b6b870757192b93cd89f541c8e61894eb4b9
nooboftime <tamara_walling@yahoo.com>
2024-07-15 22:07:37 -0500

    pep 8 in places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,7 +7,9 @@ from models.city import City
 from models.place import Place
 from models.user import User
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET'], strict_slashes=False)
 def get_places_by_city(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -26,7 +28,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
@@ -43,22 +46,22 @@ def create_place(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    
+
     if not request.is_json:
         abort(400, 'Not a JSON')
-    
+
     data = request.get_json()
-    
+
     if 'user_id' not in data:
         abort(400, 'Missing user_id')
     
     user = storage.get(User, data['user_id'])
     if not user:
         abort(404)
-    
+
     if 'name' not in data:
         abort(400, 'Missing name')
-    
+
     data['city_id'] = city_id
     new_place = Place(**data)
     new_place.save()
@@ -71,16 +74,16 @@ def update_place(place_id):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    
+
     if not request.is_json:
         abort(400, 'Not a JSON')
-    
+
     data = request.get_json()
-    
+
     ignored_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
     for key, value in data.items():
         if key not in ignored_keys:
             setattr(place, key, value)
-    
+
     place.save()
     return jsonify(place.to_dict()), 200
