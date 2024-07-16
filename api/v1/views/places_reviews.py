5efd0c725582db07c961e7b08f4430f1a62f2c1d
Tanner Saint <tsaint2004@gmail.com>
2024-07-16 19:02:14 +0000

    added user import statement

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from models import storage
 from models.place import Place
 from models.review import Review
+from models.user import User
 from flask import jsonify
 from flask import abort
 from flask import request
