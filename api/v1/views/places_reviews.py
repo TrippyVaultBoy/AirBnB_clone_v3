75040f8ea8dab1895f3ed7d77a23c1f3f1a472db
Tanner Saint <tsaint2004@gmail.com>
2024-07-16 19:03:35 +0000

    fixed PEP8 line too long

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -82,7 +82,11 @@ def update_review(review_id):
     if not request.is_json:
         abort(400, 'Not a JSON')
     for key, value in request.get_json().items():
-        if key not in ["id", "user_id", "place_id", "created_at", "updated_at"]:
+        if key not in ["id",
+                       "user_id",
+                       "place_id",
+                       "created_at",
+                       "updated_at"]:
             setattr(review, key, value)
     review.save()
     return jsonify(review.to_dict()), 200
