42eae41174a9979481193c3949c804787488b3de
Tanner Saint <tsaint2004@gmail.com>
2024-07-16 18:09:47 +0000

    removed 200

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -12,7 +12,7 @@ from flask import request
            strict_slashes=False)
 def get_amenities():
     """Retrieves the list of all Amenity objects."""
-    return jsonify([amenity.to_dict() for amenity in storage.all(Amenity).values()]), 200
+    return jsonify([amenity.to_dict() for amenity in storage.all(Amenity).values()])
 
 @app_views.route('/amenities/<amenity_id>',
                  methods=['GET'],
